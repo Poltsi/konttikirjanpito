176922c863b8df4ec4ae6bdda2f5065cf64b3988
Paul-Erik Törrönen <poltsi@poltsi.fi>
2017-09-26 08:14:21 +0300

    * Clean up and group the statements
    Signed-off-by: Paul-Erik Törrönen <poltsi@poltsi.fi>

--- sql/create_database.sql
+++ sql/create_database.sql
@@ -6,31 +6,31 @@ CREATE USER kontti WITH PASSWORD 'konttipassu';
 
 CREATE DATABASE kontti OWNER kontti;
 
+-- Grous-table
 CREATE TABLE groups (
   gid integer NOT NULL,
   name character varying(32),
   description text
 );
 
-
 ALTER TABLE groups OWNER TO kontti;
 
 COMMENT ON TABLE groups IS 'User groups';
 
-
-CREATE SEQUENCE groups_gid_seq
-START WITH 1
-INCREMENT BY 1
-NO MINVALUE
-NO MAXVALUE
-CACHE 1;
-
+CREATE SEQUENCE groups_gid_seq START WITH 1 INCREMENT BY 1 NO MINVALUE NO MAXVALUE CACHE 1;
 
 ALTER TABLE groups_gid_seq OWNER TO kontti;
-
 ALTER SEQUENCE groups_gid_seq OWNED BY groups.gid;
+ALTER TABLE ONLY groups ALTER COLUMN gid SET DEFAULT nextval('groups_gid_seq'::regclass);
+INSERT INTO groups (gid, name, description) VALUES (1,'admin', 'Administrators');
+SELECT pg_catalog.setval('groups_gid_seq', 1, true);
+ALTER TABLE ONLY groups ADD CONSTRAINT groups_name_key UNIQUE (name);
+ALTER TABLE ONLY groups ADD CONSTRAINT groups_pkey PRIMARY KEY (gid);
+
 
 SET default_with_oids = true;
+
+-- Users table
 
 CREATE TABLE users (
   uid integer NOT NULL,
@@ -45,40 +45,22 @@ CREATE TABLE users (
 ALTER TABLE users OWNER TO kontti;
 COMMENT ON TABLE users IS 'Users';
 
-CREATE SEQUENCE users_uid_seq
-START WITH 1
-INCREMENT BY 1
-NO MINVALUE
-NO MAXVALUE
-CACHE 1;
+CREATE SEQUENCE users_uid_seq START WITH 1 INCREMENT BY 1 NO MINVALUE NO MAXVALUE CACHE 1;
 
 ALTER TABLE users_uid_seq OWNER TO kontti;
-
 ALTER SEQUENCE users_uid_seq OWNED BY users.uid;
-
-ALTER TABLE ONLY groups ALTER COLUMN gid SET DEFAULT nextval('groups_gid_seq'::regclass);
-
 ALTER TABLE ONLY users ALTER COLUMN uid SET DEFAULT nextval('users_uid_seq'::regclass);
-
-INSERT INTO groups (gid, name, description) VALUES (1,'admin', 'Administrators');
-
-SELECT pg_catalog.setval('groups_gid_seq', 1, true);
-
-
 INSERT INTO users (uid, gid, login, salt, password, name, enabled)
 VALUES (1, 1, 'admin', gen_salt('bf', 8), '', 'Administrator', TRUE);
-
 UPDATE users SET password = crypt('US_admin_passu', salt) WHERE uid = 1;
-
 SELECT pg_catalog.setval('users_uid_seq', 1, false);
-
-ALTER TABLE ONLY groups ADD CONSTRAINT groups_name_key UNIQUE (name);
-ALTER TABLE ONLY groups ADD CONSTRAINT groups_pkey PRIMARY KEY (gid);
 
 ALTER TABLE ONLY users ADD CONSTRAINT users_login_key UNIQUE (login);
 ALTER TABLE ONLY users ADD CONSTRAINT users_password_key UNIQUE (password);
 ALTER TABLE ONLY users ADD CONSTRAINT users_pkey PRIMARY KEY (uid);
 ALTER TABLE ONLY users ADD CONSTRAINT users_salt_key UNIQUE (salt);
+
+-- Fills table
 
 CREATE TABLE fills (
   uid integer NOT NULL,
