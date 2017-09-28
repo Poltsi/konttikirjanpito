acd01a922432774ddca23800a20216de2145c6d6
Paul-Erik Törrönen <poltsi@poltsi.fi>
2017-09-28 07:56:42 +0300

    * Add copyright
    Signed-off-by: Paul-Erik Törrönen <poltsi@poltsi.fi>

--- src/www_root/login.php
+++ src/www_root/login.php
@@ -1,5 +1,20 @@
 <?php
 /**
+ * Copyright (c) Paul-Erik Törrönen 2017.
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation version 2 of the License and provided
+ *  that the above copyright and permission notice is included with all
+ *  distributed copies of this or derived software.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ */
+
+/**
  * Created by PhpStorm.
  * User: poltsi
  * Date: 9/25/17
