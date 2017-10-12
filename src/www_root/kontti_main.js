0220cf66fc951e6afba51db8e409695bd28c89cc
Paul-Erik Törrönen <poltsi@poltsi.fi>
2017-10-12 18:46:11 +0300

    * Refactor the back to fill-button to its own function * Add basic buttons for stats including back to fill
    Signed-off-by: Paul-Erik Törrönen <poltsi@poltsi.fi>

--- src/www_root/kontti_main.js
+++ src/www_root/kontti_main.js
@@ -94,6 +94,7 @@ function display_action_buttons() {
             show_main();
             break;
         case 'stats':
+            show_stats();
             break;
         case 'admin':
             show_admin();
@@ -256,14 +257,18 @@ function show_admin() {
     users_button.addEventListener('click', get_user_list_function());
     button_div.appendChild(users_button);
 
+    button_div.appendChild(show_back_to_fill_button());
+
+
+    data_action_elem.appendChild(button_div);
+}
+
+function show_back_to_fill_button() {
     var back_button = document.createElement('button');
     back_button.innerHTML = 'Back to fill';
     back_button.id = 'back';
     back_button.addEventListener('click', get_back_button_function());
-    button_div.appendChild(back_button);
-
-
-    data_action_elem.appendChild(button_div);
+    return back_button;
 }
 
 function get_user_list_function() {
@@ -1108,6 +1113,34 @@ function stats() {
     display_action_buttons();
     // TODO: Empty the main div
     // TODO: Fetch the stats from server
+}
+
+function show_stats() {
+    /* TODO: Populate the action div */
+    var data_action_elem = document.querySelector('#' + MAINDATAACTIONID);
+    var button_div = document.createElement('div');
+    button_div.id = 'stats_action';
+
+    /* List users */
+    var own_button = document.createElement('button');
+    own_button.innerHTML = 'Show your own stats';
+    own_button.id = 'own_stats';
+    own_button.addEventListener('click', get_user_list_function());
+    button_div.appendChild(own_button);
+
+    if (sessionStorage.getItem('kontti_level') > 40) {
+        var users_button = document.createElement('button');
+        users_button.innerHTML = 'Show your own stats';
+        users_button.id = 'users_stats';
+        users_button.addEventListener('click', get_user_list_function());
+        button_div.appendChild(users_button);
+    }
+
+    button_div.appendChild(show_back_to_fill_button());
+
+
+    data_action_elem.appendChild(button_div);
+
 }
 
 /****************************************************** /STATS  ******************************************************/
